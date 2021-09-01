53951cb71f46364495cfd3b08b7da9f1f8a2a413
Obeidareda37 <obeidareda37@gmail.com>
2021-09-01 13:56:29 +0300

    part of Image To Chat1

--- lib/auth/view/group_chat.dart
+++ lib/auth/view/group_chat.dart
@@ -3,8 +3,6 @@ import 'package:chat_app/helpers/auth_helper.dart';
 import 'package:chat_app/helpers/firestore_helper.dart';
 import 'package:chat_app/providers/auth_provider.dart';
 import 'package:chat_app/service/routes_helpers.dart';
-import 'package:chat_app/widget/custom_bottom_navigation_chat.dart';
-import 'package:chat_app/widget/custom_chat.dart';
 import 'package:chat_app/widget/custom_icon.dart';
 import 'package:chat_app/widget/custom_text.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -12,6 +10,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_chat_bubble/bubble_type.dart';
+import 'package:flutter_chat_bubble/chat_bubble.dart';
+import 'package:flutter_chat_bubble/clippers/chat_bubble_clipper_5.dart';
 
 class GroupChatPage extends StatefulWidget {
   static final routeName = 'Group';
@@ -21,46 +22,29 @@ class GroupChatPage extends StatefulWidget {
 }
 
 class _GroupChatPageState extends State<GroupChatPage> {
-  TextEditingController messageController = TextEditingController();
+  TextEditingController textEditingController = TextEditingController();
   String message;
-  List<Map> Message;
-  ScrollController controller = new ScrollController();
 
-  sendTofirestore() async {
-    FirebaseHelpers.firebaseHelpers.addMessageToFirbaseFirestore({
-      'message': messageController.text,
-      'dateTime': DateTime.now(),
-      'imageUrl':
-          Provider.of<AuthProvider>(context, listen: false).user.imageUrl,
-      'fname': Provider.of<AuthProvider>(context, listen: false).user.fname,
-    });
+  sendToFirestore() async {
+    textEditingController.clear();
+    FirebaseHelpers.firebaseHelpers.addMessageToFirbaseFirestore(
+        {'message': this.message, 'dateTime': DateTime.now()});
   }
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    messageController.dispose();
-  }
+  ScrollController scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: SystemUiOverlayStyle(
         statusBarBrightness: Brightness.light,
         statusBarIconBrightness: Brightness.dark,
         statusBarColor: Colors.white,
-        systemNavigationBarIconBrightness: Brightness.light,
-        systemNavigationBarColor: Color(0xff1E58B6),
+        systemNavigationBarIconBrightness: Brightness.dark,
+        systemNavigationBarColor: Colors.grey[200],
       ),
       child: Scaffold(
-        backgroundColor: Colors.white,
         appBar: AppBar(
           iconTheme: IconThemeData(color: Colors.black),
           leading: IconButton(
@@ -112,121 +96,137 @@ class _GroupChatPageState extends State<GroupChatPage> {
                 children: [
                   Expanded(
                     child: Container(
-                      margin: EdgeInsets.only(bottom: 20, top: 10),
                       child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
                         stream:
                             FirebaseHelpers.firebaseHelpers.getFirestoreStrem(),
                         builder: (context, datasnapshot) {
-                          Future.delayed(Duration(milliseconds: 100))
-                              .then((value) {
-                            controller.animateTo(
-                                controller.position.maxScrollExtent,
-                                duration: Duration(milliseconds: 100),
-                                curve: Curves.easeInOut);
-                          });
                           if (!datasnapshot.hasData) {
                             return Center(child: CircularProgressIndicator());
                           }
-                          QuerySnapshot<Map<String, dynamic>> querySnapShot =
+                          Future.delayed(Duration(seconds: 1)).then((value) {
+                            scrollController.animateTo(
+                                scrollController.position.maxScrollExtent,
+                                duration: Duration(milliseconds: 100),
+                                curve: Curves.easeInOut);
+                          });
+                          QuerySnapshot<Map<String, dynamic>> querySnapshot =
                               datasnapshot.data;
-                          Message =
-                              querySnapShot.docs.map((e) => e.data()).toList();
-
-                          return Message == null
-                              ? Center(
-                                  child: CircularProgressIndicator(),
-                                )
-                              : ListView.builder(
-                                  controller: controller,
-                                  shrinkWrap: true,
-                                  itemBuilder: (context, index) {
-                                    return Message[index]['userId'] ==
-                                            provider.myId
-                                        ? Directionality(
-                                            textDirection: TextDirection.rtl,
-                                            child: CustomChatBetweenTwoClient(
-                                              color: Color(0xff2978F6),
-                                              right: true,
-                                              message: Message[index]
-                                                  ['message'],
-                                              image: Message[index]
-                                                      ['imageUrl'] ??
-                                                  'https://i.pinimg.com/originals/70/c7/af/70c7afacdfb4016f786753f9e5071fc4.jpg',
-                                              name: Message[index]['fname'] ??
-                                                  'Guest',
+                          List<Map> messages =
+                              querySnapshot.docs.map((e) => e.data()).toList();
+                          return ListView.builder(
+                              shrinkWrap: true,
+                              controller: scrollController,
+                              itemCount: messages.length,
+                              itemBuilder: (context, index) {
+                                bool isMe =
+                                    messages[index]['userId'] == provider.myId;
+                                return isMe
+                                    ? Container(
+                                        child: ChatBubble(
+                                          shadowColor: Colors.transparent,
+                                          alignment: Alignment.topRight,
+                                          margin: EdgeInsets.only(
+                                              top: 20, right: 20, bottom: 10),
+                                          backGroundColor: messages[index]
+                                                      ['imageUrl'] ==
+                                                  null
+                                              ? Colors.blue
+                                              : Colors.transparent
+                                                  .withOpacity(0),
+                                          child: Container(
+                                              constraints: BoxConstraints(
+                                                  maxWidth:
+                                                      MediaQuery.of(context)
+                                                              .size
+                                                              .width *
+                                                          0.7),
+                                              child: messages[index]
+                                                          ['imageUrl'] ==
+                                                      null
+                                                  ? Text(
+                                                      messages[index]
+                                                          ['message'],
+                                                      style: TextStyle(
+                                                          color: Colors.white))
+                                                  : Image.network(
+                                                      messages[index]
+                                                          ['imageUrl'])),
+                                          clipper: ChatBubbleClipper5(
+                                              type: BubbleType.sendBubble),
+                                        ),
+                                      )
+                                    : ChatBubble(
+                                        backGroundColor: Color(0xffE7E7ED),
+                                        margin: EdgeInsets.only(
+                                            top: 20, left: 20, bottom: 10),
+                                        child: Container(
+                                            constraints: BoxConstraints(
+                                              maxWidth: MediaQuery.of(context)
+                                                      .size
+                                                      .width *
+                                                  0.7,
                                             ),
-                                          )
-                                        : Directionality(
-                                            textDirection: TextDirection.ltr,
-                                            child: CustomChatBetweenTwoClient(
-                                              textDirection: TextDirection.ltr,
-                                              right: false,
-                                              color: Color(0xffE4E4E4),
-                                              message: Message[index]
-                                                  ['message'],
-                                              image: Message[index]
-                                                      ['imageUrl'] ??
-                                                  'https://i.pinimg.com/originals/70/c7/af/70c7afacdfb4016f786753f9e5071fc4.jpg',
-                                              name: Message[index]['fname'] ??
-                                                  'Guest',
-                                            ),
-                                          );
-                                  },
-                                  itemCount: Message.length,
-                                );
+                                            child: messages[index]
+                                                        ['imageUrl'] ==
+                                                    null
+                                                ? Text(
+                                                    messages[index]['message'],
+                                                    style: TextStyle(
+                                                        color: Colors.black),
+                                                  )
+                                                : Image.network(messages[index]
+                                                    ['imageUrl'])),
+                                        clipper: ChatBubbleClipper5(
+                                            type: BubbleType.receiverBubble),
+                                      );
+                              });
                         },
                       ),
                     ),
                   ),
                   Container(
-                    color: Color(0xff1E58B6),
-                    width: double.infinity,
-                    padding: EdgeInsets.all(8),
+                    margin: EdgeInsets.symmetric(horizontal: 10),
                     child: Row(
                       children: [
                         Expanded(
                           child: Container(
-                            margin: EdgeInsets.symmetric(horizontal: 5),
-                            child: TextFormField(
-                              maxLines: 10,
-                              minLines: 1,
-                              controller: messageController,
-                              keyboardType: TextInputType.text,
-                              textAlign: TextAlign.start,
-                              onChanged: (x) {},
-                              decoration: InputDecoration(
-                                  hintText: 'Enter a message',
-                                  filled: true,
-                                  fillColor: Colors.white,
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(15))),
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.black),
+                                borderRadius: BorderRadius.circular(15)),
+                            child: Row(
+                              children: [
+                                IconButton(
+                                    onPressed: () {
+                                      provider.sendImageToChat();
+                                    },
+                                    icon: Icon(Icons.attach_file)),
+                                Expanded(
+                                    child: TextField(
+                                  decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                          borderSide: BorderSide.none)),
+                                  controller: textEditingController,
+                                  onChanged: (x) {
+                                    this.message = x;
+                                  },
+                                )),
+                              ],
                             ),
                           ),
                         ),
                         Container(
-                          margin: EdgeInsets.only(right: 10),
-                          child: IconButton(
-                            icon: Icon(Icons.attach_file),
-                            onPressed: () {
-                              provider.sendImageToChat();
-                            },
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(right: 10),
+                          decoration: BoxDecoration(
+                              color: Color(0xff1E58B6),
+                              borderRadius: BorderRadius.circular(15)),
+                          padding: EdgeInsets.all(20),
+                          margin: EdgeInsets.all(5),
                           child: CustomIcon(
                             image: 'assets/icons/send.svg',
-                            width: 24,
-                            height: 23,
-                            onTap: () {
-                              sendTofirestore();
-                              messageController.clear();
-                            },
                           ),
                         ),
                       ],
                     ),
-                  ),
+                  )
                 ],
               ),
             );
